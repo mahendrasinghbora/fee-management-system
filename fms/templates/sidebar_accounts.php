24206de65a092307e1b2a63ee4d7806217b7f81d
Mahendra Singh Bora <Mahendraeminent@gmail.com>
2021-06-09 12:40:11 +0530

    Add templates

---
+++ fms/templates/sidebar_accounts.php
@@ -0,0 +1,17 @@
+<!-- sidebar accounts -->
+<nav class="col-sm-3 col-md-2 d-none d-md-block bg-light sidebar border border-dark border-left-0 border-bottom-0 border-top-0">
+	<ul class="nav nav-pills flex-column pt-5">
+		<li class="nav-item">
+			<a class="nav-link" href="#">Overview <span class="sr-only">(current)</span></a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Reports</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Analytics</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Export</a>
+		</li>
+	</ul>
+</nav><!-- /sidebar accounts -->
